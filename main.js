1d094bb309a1cabf61089c73b6e3983af947f95e
Lena <olena.ratushna21@gmail.com>
2022-09-09 14:45:28 +0300

    Added removing notes

--- main.js
+++ main.js
@@ -10,15 +10,34 @@ if (notesFromLocalStorage) {
 
 function renderList(list) {
 	let listItems = '';
-	list.forEach((i) => (listItems += `<li class="list-item">${i}</li>`));
+	list.forEach((i) => {
+        listItems += `
+            <li class="list-item">
+                <span>${i}</span>
+                <button class="delete-btn" data-action="delete">✖</button>
+            </li>
+        `
+    });
 	savedList.innerHTML = listItems;
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('notesLits', JSON.stringify(notesList));
 }
 
 input.addEventListener('keypress', (e) => {
 	if (e.key === 'Enter' && input.value) {
 		notesList.push(input.value);
 		input.value = '';
-		localStorage.setItem('notesLits', JSON.stringify(notesList));
+		updateLocalStorage();
 		renderList(notesList);
 	}
+});
+
+//delete note
+savedList.addEventListener('click', (e) => {
+    if(e.target.dataset.action === 'delete') {
+        const parentNode = e.target.closest('.list-item');
+        parentNode.remove();
+    }
 });
